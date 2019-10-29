72ed168095a28c5e00d9909184adbbef0f783dbb
enteitsuki <enteitsuki@gmail.com>
2019-10-29 20:23:25 +0900

    calc_app

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,4 +1,20 @@
 class CalcsController < ApplicationController
+  def result
+    val1 = params[:val1]
+    calculation = params[:calculation]
+    val2 = params[:val2]
+    case calculation
+    when "addition"
+      @result = val1.to_i + val2.to_i
+    when "subtraction"
+      @result = val1.to_i - val2.to_i
+    when "multiplication"
+      @result = val1.to_i * val2.to_i
+    when "division"
+      @result = val1.to_f / val2.to_f
+    end
+  end
+
   def show
     @msg1 = params[:msg1]
     @msg2 = params[:msg2]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'calcs/:val1/:calculation/:val2' => 'calcs#result'
   get 'calcs/:msg1/:msg2/:msg3' => 'calcs#show'
 end
