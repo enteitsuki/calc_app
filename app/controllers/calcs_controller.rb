9833cbf010c82bc329ef5d451feee0b8d30eb225
enteitsuki <enteitsuki@gmail.com>
2019-11-02 21:20:02 +0900

    calc_app2

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -14,21 +14,4 @@ class CalcsController < ApplicationController
       @result = val1.to_f / val2.to_f
     end
   end
-
-  def show
-    @msg1 = params[:msg1]
-    @msg2 = params[:msg2]
-    @msg3 = params[:msg3]
-    # @msg = "#{:msg1}と#{:msg2}と#{:msg2}"
-    case @msg2
-    when "addition"
-      @msg = @msg1.to_i + @msg3.to_i
-    when "subtraction"
-      @msg = @msg1.to_i - @msg3.to_i
-    when "multiplication"
-      @msg = @msg1.to_i * @msg3.to_i
-    when "division"
-      @msg = @msg1.to_i / @msg3.to_i
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'calcs/:val1/:calculation/:val2' => 'calcs#result'
-  get 'calcs/:msg1/:msg2/:msg3' => 'calcs#show'
 end
